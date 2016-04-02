be23076f93747e149872e36345653877a8708a04
Z-studio <z.electrostudio@gmail.com>
2016-04-02 15:03:29 +0300

    lesson 27

---
+++ lessons/zslib/cpp/build/CMakeFiles/opus_encode.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/zstudio/Documents/pd/lessons/zslib/cpp/pdopus.c" "/Users/zstudio/Documents/pd/lessons/zslib/cpp/build/CMakeFiles/opus_encode.dir/pdopus.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/Applications/Pd-extended.app/Contents/Resources/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zstudio/Documents/pd/lessons/zslib/cpp/stream.cpp" "/Users/zstudio/Documents/pd/lessons/zslib/cpp/build/CMakeFiles/opus_encode.dir/stream.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/Applications/Pd-extended.app/Contents/Resources/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lessons/zslib/cpp/build/CMakeFiles/opus_encode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opus_encode.dir/pdopus.c.o"
+  "CMakeFiles/opus_encode.dir/stream.cpp.o"
+  "opus_encode.pdb"
+  "opus_encode.pd_darwin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opus_encode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
