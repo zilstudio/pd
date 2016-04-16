3cb5d5f679b8144ade30a87bd9f4b4e614cba838
Serge Poltavski <serge.poltavski@gmail.com>
2016-04-16 22:50:39 +0300

    lesson 30 update

--- lessons/zslib/cpp/udp_receive.cpp
+++ lessons/zslib/cpp/udp_receive.cpp
@@ -4,21 +4,29 @@
 #include <boost/bind.hpp>
 #include <boost/system/error_code.hpp>
 #include <boost/cstdlib.hpp>
+#include <opus/opus.h>
+#include <assert.h>
 
 using boost::asio::ip::udp;
 
 enum { max_length = 1024 };
 
-void server(boost::asio::io_service& io_service, unsigned short port){
+void server(boost::asio::io_service& io_service, unsigned short port, OpusDecoder * dec){
     udp::socket sock(io_service, udp::endpoint(udp::v4(), port));
     sock.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+
+    opus_int16 pcm[240];
 
     for (;;) {
         boost::array<char, 1024> recv_buf;
         udp::endpoint sender_endpoint;
         size_t len = sock.receive_from(
                     boost::asio::buffer(recv_buf), sender_endpoint);
-        std::cout.write(recv_buf.data(), len);
+
+        int bytes = opus_decode(dec, (unsigned char*)recv_buf.data(), len, pcm, 240, 0);
+        std::cout.write((char*)pcm, bytes * sizeof(opus_int16));
+
+        //        std::cout.write(recv_buf.data(), len);
     }
 }
 
@@ -29,8 +37,12 @@ int main(int argc, char *argv[]) {
             return boost::exit_failure;
         }
 
+        int err = 0;
+        OpusDecoder * decoder = opus_decoder_create(48000, 1, &err);
+        assert(err == 0);
+
         boost::asio::io_service io_service;
-        server(io_service, std::atoi(argv[1]));
+        server(io_service, std::atoi(argv[1]), decoder);
     }
     catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << "\n";
--- lessons/zslib/cpp/udp_send_broadcast~.c
+++ lessons/zslib/cpp/udp_send_broadcast~.c
@@ -1,8 +1,13 @@
 #include <m_pd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <opus/opus.h>
+#include <string.h>
 
 #include "stream.h"
+#include "encoder.h"
+
+#define ENCODER_FRAME_SIZE 240
 
 static t_class * udp_send_broadcast_tilde_class;
 
@@ -10,6 +15,10 @@ typedef struct _opus {
     t_object  x_obj;
     t_float f;
     Streamer * streamer;
+    OpusEncoder * encoder;
+    t_sample in_buf[ENCODER_FRAME_SIZE];
+    int int_counter;
+    unsigned char out_buf[4000];
 } t_udp_send_broadcast_tilde;
 
 void *udp_send_broadcast_tilde_new(t_floatarg port)
@@ -19,11 +28,45 @@ void *udp_send_broadcast_tilde_new(t_floatarg port)
     outlet_new(&x->x_obj, &s_float);
 
     x->streamer = streamer_create("255.255.255.255", port);
+    int err = 0;
+    x->encoder = opus_encoder_create(48000, 1, OPUS_APPLICATION_AUDIO, &err);
+
+    if (err < 0){
+        error("[udp_send_broadcast~] failed to create an encoder: %s\n", opus_strerror(err));
+        return 0;
+    }
+
+    err = opus_encoder_ctl(x->encoder, OPUS_SET_BITRATE(128000));
+    if (err < 0) {
+        error("[udp_send_broadcast~] failed to set bitrate: %s\n", opus_strerror(err));
+        return 0;
+    }
+
+    err = opus_encoder_ctl(x->encoder, OPUS_SET_VBR_CONSTRAINT(1));
+    if(err != OPUS_OK){
+        error("[udp_send_broadcast~] Error OPUS_SET_VBR_CONSTRAINT returned: %s\n", opus_strerror(err));
+        return 0;
+    }
+
+    err = opus_encoder_ctl(x->encoder, OPUS_SET_COMPLEXITY(4));
+    if(err != OPUS_OK){
+        error("[udp_send_broadcast~] Error OPUS_SET_COMPLEXITY returned: %s\n", opus_strerror(err));
+        return 0;
+    }
+
+//    ret=opus_multistream_encoder_ctl(st, OPUS_SET_PACKET_LOSS_PERC(expect_loss));
+//    if(ret!=OPUS_OK){
+//        fprintf(stderr,"Error OPUS_SET_PACKET_LOSS_PERC returned: %s\n",opus_strerror(ret));
+//        exit(1);
+//    }
+
+    x->int_counter = 0;
 
     return (void *)x;
 }
 
 void udp_send_broadcast_tilde_free(t_udp_send_broadcast_tilde * x) {
+    opus_encoder_destroy(x->encoder);
     streamer_destroy(x->streamer);
 }
 
@@ -32,7 +75,19 @@ t_int *udp_send_broadcast_tilde_perform(t_int *w) {
     t_sample *in1 = (t_sample*)(w[2]);
     int n = (int)(w[3]);
 
-    streamer_sendto(x->streamer, in1, n * sizeof(t_sample));
+    while(n--) {
+        if(x->int_counter == (ENCODER_FRAME_SIZE - 1)) {
+            int nbBytes = opus_encode_float(x->encoder, x->in_buf, ENCODER_FRAME_SIZE, x->out_buf, sizeof(x->out_buf));
+            if (nbBytes < 0) {
+                error("encode failed: %s\n", opus_strerror(nbBytes));
+            }
+            //            post("%i", nbBytes);
+            streamer_sendto(x->streamer, x->out_buf, nbBytes);
+            x->int_counter = 0;
+        }
+
+        x->in_buf[x->int_counter++] = *in1++;
+    }
 
     return (w+4);
 }
@@ -47,12 +102,12 @@ void udp_send_broadcast_tilde_dsp(t_udp_send_broadcast_tilde *x, t_signal **sp) 
 
 void udp_send_broadcast_tilde_setup(void) {
     udp_send_broadcast_tilde_class = class_new(gensym("udp_send_broadcast~"),
-                                     (t_newmethod) udp_send_broadcast_tilde_new,
-                                     (t_method) udp_send_broadcast_tilde_free,
-                                     sizeof(t_udp_send_broadcast_tilde),
-                                     CLASS_DEFAULT,
-                                     A_DEFFLOAT,
-                                     0);
+                                               (t_newmethod) udp_send_broadcast_tilde_new,
+                                               (t_method) udp_send_broadcast_tilde_free,
+                                               sizeof(t_udp_send_broadcast_tilde),
+                                               CLASS_DEFAULT,
+                                               A_DEFFLOAT,
+                                               0);
 
     class_addmethod(udp_send_broadcast_tilde_class,
                     (t_method)udp_send_broadcast_tilde_dsp, gensym("dsp"), 0);
